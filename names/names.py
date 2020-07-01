053731ab32171df7add1396d21d9b1194a6d29b9
Amber-Pittman <amber@amberpittman.com>
2020-07-01 12:47:40 -0400

    Names returns duplicates and runtime. MVP met.

--- names/names.py
+++ names/names.py
@@ -1,6 +1,5 @@
 import time
 
-
 class BSTNode:
     def __init__(self, value):
         self.value = value
@@ -38,20 +37,25 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []  # Return the list of duplicates in this data structure
-bst = BSTNode("")
+bst = BSTNode(100000)
 # Replace the nested for loops below with your improvements
 for name_1 in names_1:
+    name = int("".join(str(ord(char)) for char in name_1))
     bst.insert(name)
     
 for name_2 in names_2:
+    name = int("".join(str(ord(char)) for char in name_2))
     if bst.contains(name):
-        duplicates.append(name)
+        duplicates.append(name_2)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
 
-# ---------- Stretch Goal -----------
-# Python has built-in tools that allow for a very efficient approach to this problem
-# What's the best time you can accomplish?  There are no restrictions on techniques or data
-# structures, but you may not import any additional libraries that you did not write yourself.
+'''
+ ---------- Stretch Goal -----------
+ Python has built-in tools that allow for a very efficient approach to this problem
+
+ What's the best time you can accomplish?  There are no restrictions on techniques or data structures, but you may not import any additional libraries that you did not write yourself.
+
+'''
