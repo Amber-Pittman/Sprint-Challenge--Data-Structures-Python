6be478e2ea6b41235f1d376f1a45ee2b404744b1
Amber-Pittman <amber@amberpittman.com>
2020-08-28 20:25:31 -0400

    Names returns duplicates & runtimes. MVP met.

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -39,9 +39,29 @@ class LinkedList:
         return False
 
     def reverse_list(self, node, prev):
-        while node is not None:
-            next_node = node.get_next()
+        # while node is not None:
+        #     next_node = node.get_next()
+        #     node.next_node = prev
+        #     prev = node
+        #     node = next_node
+        # self.head = prev
+
+        # Easier to understand option
+        # if head is None, return
+        if node is None:
+            return
+        # if the node after is None, node is the tail
+        if node.next_node is None:
+            # set the head to the tail node, now called "node"
+            self.head = node
+            # set the next node to the "prev" passed in value
+            node.next_node = prev 
+            # return the node
+            return node
+        else:
+            # set the next node in the line
+            next = node.next_node
+            # set the node's next_node to the "prev" passed in node
             node.next_node = prev
-            prev = node
-            node = next_node
-        self.head = prev
+            # recurse through the list
+            self.reverse_list(next, node)
